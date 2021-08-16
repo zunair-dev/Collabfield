963d12889da678c117e86afc09c66cb764e17bd7
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-08-16 18:04:32 +0500

    Add CSS to posts on the home page - add CSS to the posts.scss file - add CSS to the mobile.scss to fix too large text issues on smaller screens

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,16 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+// require("jquery")
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
 //= require jquery
 //= require bootstrap-sprockets
+
+import "posts/style.js"
