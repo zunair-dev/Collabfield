10a6dabcfb4a822a6cc6c82daf45f7444a31baff
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-08-16 13:55:14 +0500

    Change sign up page's route from /users/sign_up to /signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
-    devise_scope :user do
+
+  devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
   root to: "pages#index"
   # root to: "devise/sessions#new"
 end
