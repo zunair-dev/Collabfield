77194634a4ded371e69f8426397a21bad0412f10
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-08-17 16:47:48 +0500

    Add validations to the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :content, presence: true, length: { minimum: 20, maximum: 1000 }
+  validates :category_id, presence: true
+
   belongs_to :user
   belongs_to :category
 
